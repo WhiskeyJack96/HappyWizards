a54b144cc3e6f89e956fee6bfb0553ef8aebc71b
Jacob Mikesell <jacob.mikesell96@gmail.com>
2016-12-12 13:21:28 -0800

    Walking Script
    Added filler art and a basic walking script

---
+++ HappyWizards/Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+		var y = Input.GetAxis("Vertical") *speed*Time.deltaTime;
+		var x = Input.GetAxis("Horizontal") *speed*Time.deltaTime;
+
+		transform.Translate(x,y,0);
+
+	}
+}
