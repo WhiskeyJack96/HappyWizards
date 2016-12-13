62b3bb0e37f57b904b96f4196ac9f413fea2f72c
Jacob Mikesell <jacob.mikesell96@gmail.com>
2016-12-12 17:14:11 -0800

    Clock UI
    Added a Clock to the UI.

---
+++ HappyWizards/Assets/Scripts/ClockController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+public class ClockController : MonoBehaviour {
+
+	DateTime time;
+	public Text timeText;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time = System.DateTime.Now;
+		timeText.text = time.Hour + ":" + time.Minute;
+	}
+}
