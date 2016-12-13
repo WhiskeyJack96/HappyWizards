34e2ce018f9e4fbe3ac010f90fd91ab5188ce8e1
Jacob Mikesell <jacob.mikesell96@gmail.com>
2016-12-12 18:57:23 -0800

    Smoothed out Movement added a Shrub

--- HappyWizards/Assets/Scripts/Movement.cs
+++ HappyWizards/Assets/Scripts/Movement.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
+	Rigidbody2D rb ;
+
 	public float speed;
 
+	void Start(){
+		rb = GetComponent<Rigidbody2D>();
+	}
 	// Update is called once per frame
-	void Update () {
-		var y = Input.GetAxis("Vertical") *speed*Time.deltaTime;
-		var x = Input.GetAxis("Horizontal") *speed*Time.deltaTime;
+	void FixedUpdate () {
+		var y = Input.GetAxis("Vertical") *speed;//*Time.deltaTime;
+		var x = Input.GetAxis("Horizontal") *speed;//*Time.deltaTime;
 
-		transform.Translate(x,y,0);
+		rb.velocity = new Vector3(x,y,0);
 
+//		transform.Translate(x,y,0);
+
+	}
+
+	void OnCollisionEnter(Collision other) {
+		GetComponent<Rigidbody2D>().velocity = Vector3.zero;
 	}
 }
