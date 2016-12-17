f49ba90dba62024bd000180530a97cdf4d8ed224
Jacob Mikesell <jacob.mikesell96@gmail.com>
2016-12-17 11:22:09 -0800

    Rudimentary NPC controller Script

---
+++ HappyWizards/Assets/Scripts/NPCController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NPCController : MonoBehaviour {
+
+	public String name;
+	public String dialogue;
+	
+	public List<String> inventory;
+
+	public List<Transform> waypoints;
+
+	public int affinity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
