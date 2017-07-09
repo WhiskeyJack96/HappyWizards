f3204be37c283cb9df0b989bae183153de208b9e
WhikseyJack <jacob.mikesell96@gmail.com>
2017-07-09 16:10:48 -0700

    Testing some raycast updates
    testng to see if raycasts can interact with tile map

---
+++ HappyWizards/Assets/Scripts/Temp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour {
+
+	public Transform transform;
+	public Vector3 v2;
+	public bool yes;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(yes){
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, v2);
+		if(hit){
+		Debug.Log(hit.transform.parent.parent.name);
+	}}}
+}
